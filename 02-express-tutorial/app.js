ad7546a4f2fd6e6775d6f3fb7b88b471387609a9
Rohithprakashdiv <Rohithprakash19@gmail.com>
2025-07-11 22:57:14 +0530

    Add new backend features and cleanup

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,28 @@
-console.log('Express Tutorial')
+const http = require('http')
+
+const {readFileSync} = require('fs')
+
+const homePage = readFileSync('./index.html') 
+
+const server = http.createServer((req,res)=>{
+    const url  = req.url
+
+    if(url=='/'){
+        res.writeHead(200, { 'Content-Type': 'text/html' })
+       res.write(homePage)
+        res.end()
+    }else if(url==='/about'){
+        res.writeHead(200,{'Content-Type': 'application/json'})
+        res.write('<h1>this is about page</h1>')
+        res.end()
+    }else{
+        res.writeHead(404,{'Content-Type': 'application/json'})
+        res.write('<h1>this is error page</h1>')
+        res.end()
+
+    }
+
+
+})
+
+server.listen(5000)
