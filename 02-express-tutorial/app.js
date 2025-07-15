d70488e258ee7501b4c93b94017b4a78136bd019
Rohithprakashdiv <Rohithprakash19@gmail.com>
2025-07-15 21:17:04 +0530

    🔧 Add express.urlencoded middleware to parse form data

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,64 +1,35 @@
 const express = require('express')
-const path = require('path')
 const app = express()
-const {products} = require('./data')
+const middle = require('./middleware')
+const  morgan = require('morgan')
+const {people} = require('./data')
 
+// app.use(morgan('tiny'))
+// app.use(middle)
 
-// app.use(express.static('./public'))
+app.use(express.static('./methods-public'))
+
+app.use(express.urlencoded({extended:false}))
 
 app.get('/', (req, res) => {
-  res.send('<h1> Home Page</h1><a href="/api/products">products</a>')
+  res.status(200).json({success:true,data:people})
 })
-app.get('/api/products',(req,res)=>{
-
-    const newProducts = products.map((product)=>{
-        const {id,name,image}=product
-        return {id,name,image}
-    })
-
-    res.json(newProducts)
-
-})
-app.get('/api/products/:productID',(req,res)=>{
-
- const {productID} = req.params
-//  console.log(productID);
-  
-const singleProduct= products.find((product)=>product.id ===Number(productID))
-
- if(!singleProduct){
-    return res.status(404).send('product not Found')
+app.post('/login', (req, res) => {
+  console.log(req.body);
+ const {name} = req.body
+ if(name){
+ res.status(200).send(`hi ${name}`); 
  }
-
- return res.json(singleProduct)
-
+  res.status(401).send('enter name')
 })
-
-app.get('/api/vi/query',(req,res)=>{
-
-    const {search,limit} =req.query;
-
-    let sortedProducts = [...products]
-
-  if(search){
-    sortedProducts = sortedProducts.filter((products)=>{
-        return products.name.startsWith(search)
-    })
-  }
-  if(limit){
-    sortedProducts = sortedProducts.slice(0,Number(limit))
-  }
-    res.status(200).json(sortedProducts)
-})
-
-
-
-
-// app.all('/*splat',(req,res)=>{ 
-// res.status(404).send('<h1>404 Page Not Found</h1>');
+// app.get('/api/products', (req, res) => {
+//   res.send('Products')
 // })
-
+// app.get('/api/items', (req, res) => {
+//   console.log(req.user)
+//   res.send('Items')
+// })
 app.listen(5000,()=>{
-    console.log('listen in 5000');
-    
+  console.log('listen in 5000');
+  
 })
